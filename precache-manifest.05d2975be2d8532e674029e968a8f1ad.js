117e9fbe963d2b3b0f4502545f4736bc43d34fd8
abdullah <hbkamk@gmail.com>
2020-07-17 18:40:01 +0600

    Updates

---
+++ precache-manifest.05d2975be2d8532e674029e968a8f1ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d17ac55aacdd75ac3229145b658d2e",
+    "url": "/simple-weather-app/index.html"
+  },
+  {
+    "revision": "97f456153dbb488a8438",
+    "url": "/simple-weather-app/static/css/main.2d905351.chunk.css"
+  },
+  {
+    "revision": "6f9da207b79e184da8ff",
+    "url": "/simple-weather-app/static/js/2.095184d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-weather-app/static/js/2.095184d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f456153dbb488a8438",
+    "url": "/simple-weather-app/static/js/main.c371a2d7.chunk.js"
+  },
+  {
+    "revision": "6018d975febee4b7ae08",
+    "url": "/simple-weather-app/static/js/runtime-main.ea2d7dcf.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/simple-weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/simple-weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
